6b809f4772115c7dae9ee86b3117a45625fc456a
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-02-23 20:37:55 -0800

    adding namespace for v1 and configuring routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :companies
-  resources :rails
-  resources :employers
+  scope module: :v1 do
+    resources :companies do
+      resources :projects
+    end
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
