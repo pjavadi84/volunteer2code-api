b6989afccb272d758d5e4e0019e43431bcee84e3
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-02-23 20:56:55 -0800

    add column title and removed type column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_173754) do
+ActiveRecord::Schema.define(version: 2021_02_24_044716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,11 +30,11 @@ ActiveRecord::Schema.define(version: 2021_02_23_173754) do
   create_table "projects", force: :cascade do |t|
     t.integer "company_id"
     t.text "project_description"
-    t.string "type"
     t.date "deadline"
     t.string "reward"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
   end
 
 end
